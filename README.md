c4ed9daa3b380d5ab74b5d1930c8fec9287d9a3d
Bruno Tarditi <brunotarditi@hotmail.com>
2021-06-13 20:23:39 -0300

    TP 8 LAB 4 finalizado

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# Trabajo-Practico-8-LAB4-UTN
-CRUD Vue with API instrumentos
+# trabajo-practico-8-frontend
+
+```
+CRUD vue with API instrumentos
+```
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/Productos.vue
@@ -0,0 +1,41 @@
+<template>
+  <div style="margin-top:80px">
+    <div
+      v-for="instrumento in instrumentos"
+      :key="instrumento.id"
+      style="margin-top:15px"
+      class="d-flex justify-content-center"
+    >
+      <instrumento-item :instrumentoParam="instrumento"></instrumento-item>
+    </div>
+  </div>
+</template>
+
+<script>
+import Instrumento from "@/components/Instrumento.vue";
+import axios from "axios";
+export default {
+  name: "Productos",
+  components: {
+    "instrumento-item": Instrumento,
+  },
+  mounted() {
+    this.getInstrumentos();
+  },
+  data() {
+    return {
+      instrumentos: [],
+    };
+  },
+  methods: {
+    getInstrumentos() {
+      axios.get("http://localhost:8080/instrumento").then((res) => {
+        console.log(res.data);
+        this.instrumentos = res.data;
+      }).catch((err) => {
+          console.log(err)
+      });
+    },
+  },
+};
+</script>
